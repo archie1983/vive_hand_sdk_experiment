86bca7a487ac9bdfb5102ea8f802455cdd3965c5
Arturs Elksnis <archie1983@hotmail.co.uk>
2022-04-24 13:20:09 +0100

    First commit. Hands can be detected and position of thumbs up of one hand cand be harvested.

---
+++ Assets/CameraFeed.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFeed : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamTexture wct = new WebCamTexture();
+        WebCamDevice[] devices = WebCamTexture.devices;
+        wct.deviceName = devices[0].name;
+        this.GetComponent<MeshRenderer>().material.mainTexture = wct;
+        wct.Play();
+    }
+}
---
+++ Assets/CustomGrab.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ViveHandTracking;
+
+    public class CustomGrab : MonoBehaviour
+{
+    private Rigidbody target = null;
+    private int state = 0;
+    //private bool exit = true;
+
+    public Text txtCoord;
+    private static int SCALE = 10;
+
+    void Awake()
+    {
+        Debug.Log("AWAKE");
+    }
+
+    void Update()
+    {
+        if (GestureProvider.LeftHand != null && GestureProvider.LeftHand.gesture == GestureType.Like)
+        {
+            txtCoord.text = string.Format("{0:0.##}", GestureProvider.LeftHand.position.x * SCALE) 
+                + " # " + string.Format("{0:0.##}", GestureProvider.LeftHand.position.y * SCALE)
+                + " # " + string.Format("{0:0.##}", GestureProvider.LeftHand.position.z * SCALE)
+                + "\n" + GestureProvider.LeftHand.rotation.ToString();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TRIG ENTER");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("TRIG EXIT");
+        if (other.GetComponent<Rigidbody>() != target) return;
+        //if (state == 1) exit = true;
+    }
+
+    public void OnStateChanged(int state)
+    {
+        Debug.Log("OK detected" + (txtCoord == null));
+/*        if (GestureProvider.LeftHand != null && GestureProvider.LeftHand.gesture == GestureType.Like)
+        {
+            txtCoord.text = GestureProvider.LeftHand.position.x + " # " + GestureProvider.LeftHand.position.y + " # " + GestureProvider.LeftHand.position.z;
+        }*/
+
+        this.state = state;
+    }
+}
